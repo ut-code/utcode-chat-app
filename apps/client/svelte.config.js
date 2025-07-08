cc866a7ba14cad917e4b991f16ff16d7867b8d95
aster <137767097+aster-void@users.noreply.github.com>
2025-07-08 15:19:45 +0900

    fix some config

--- apps/client/svelte.config.js
+++ apps/client/svelte.config.js
@@ -9,10 +9,11 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      pages: "./dist",
+      assets: "./dist",
+    }),
+    outDir: "./.svelte-kit",
     env: {
       dir: "../../",
     },
@@ -20,7 +21,6 @@ const config = {
       $components: "src/components",
       "~": "src/",
     },
-    outDir: "./dist",
   },
 };
 
