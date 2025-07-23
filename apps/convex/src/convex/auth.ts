3f375dbbf9b131ec912084ad2bfb8389ca31ff68
nakomochi <nakomochi13@gmail.com>
2025-07-23 21:03:26 +0900

    Googleでの認証

--- apps/convex/src/convex/auth.ts
+++ apps/convex/src/convex/auth.ts
@@ -2,7 +2,11 @@ import { Password } from "@convex-dev/auth/providers/Password";
 import { convexAuth } from "@convex-dev/auth/server";
 import { ResendOTPPasswordReset } from "./ResendOTPPasswordReset";
 import { ResendOTP } from "./ResendOTP";
+import Google from "@auth/core/providers/google";
 
 export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Password({ reset: ResendOTPPasswordReset, verify: ResendOTP })],
+  providers: [
+    Password({ reset: ResendOTPPasswordReset, verify: ResendOTP }),
+    Google,
+  ],
 });
